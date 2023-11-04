5d585a42a5ec178e0f244d9314f0d091396e6d3d
justinelut <justinequartz@gmail.com>
2023-11-04 13:52:35 +0000

    A camping temlate with nextjs

---
+++ README.md
@@ -0,0 +1,45 @@
+# Camping Adventure Template
+
+Welcome to the Camping Adventure Template, a beautifully designed web template for camping enthusiasts. This template is created with love and care by [Your Name] and the Pixelayout team.
+
+## Overview
+
+The Camping Adventure Template is the perfect solution for creating websites dedicated to camping and outdoor adventures. Whether you're a campground owner, outdoor equipment retailer, or simply a nature lover, this template will help you showcase the beauty of the great outdoors and your offerings.
+
+## Features
+
+- **Stunning Design:** The template features a visually appealing design that highlights the beauty of nature and the camping experience.
+
+- **Responsive:** The template is fully responsive, ensuring it looks great on all devices, from desktops to mobile phones.
+
+- **Easy Customization:** Customize the template to suit your specific needs with ease, thanks to its well-organized structure and code.
+
+- **High Performance:** The template is optimized for performance, ensuring fast loading times and a smooth user experience.
+
+- **Outdoor Adventures:** Showcase various camping and outdoor activities, from hiking and fishing to stargazing and campfires.
+
+- **Photo Galleries:** Feature beautiful photo galleries to give your visitors a taste of the camping experience.
+
+## Getting Started
+
+To get started with the Camping Adventure Template, follow these steps:
+
+1. **Clone the Repository:** Clone this repository to your local machine from our [GitHub repository](https://github.com/justinelut/camping).
+
+2. **Customize:** Modify the template to your liking. You can replace the demo content with your own text and images.
+
+3. **Testing:** Test the template on different devices to ensure it's responsive.
+
+4. **Deployment:** Deploy the template to your hosting server.
+
+5. **Enjoy:** Your camping website is now live and ready to inspire your visitors!
+
+## License
+
+This template is licensed under the [Your License] License. You can find more information in the `LICENSE` file.
+
+## Support
+
+If you encounter any issues or have questions about the Camping Adventure Template, feel free to reach out to us at [support@pixelayout.site](mailto:support@pixelayout.site) or call us at +254740455200.
+
+Happy camping, and enjoy using the template!
---
+++ constants/index.ts
@@ -0,0 +1,95 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: '/', key: 'home', label: 'Home' },
+  { href: '/', key: 'how_hilink_work', label: 'How it works?' },
+  { href: '/', key: 'services', label: 'Services' },
+  { href: '/', key: 'pricing ', label: 'Pricing ' },
+  { href: '/', key: 'contact_us', label: 'Contact Us' },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+  '/person-1.png',
+  '/person-2.png',
+  '/person-3.png',
+  '/person-4.png',
+];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: 'Offline Access to Real Maps',
+    icon: '/map.svg',
+    variant: 'green',
+    description:
+      'PixelTravel offers the convenience of using our application with offline maps, ensuring you can navigate even in areas with no signal.',
+  },
+  {
+    title: 'Plan Adventure Schedules',
+    icon: '/calendar.svg',
+    variant: 'green',
+    description:
+      'Schedule exciting adventures with friends using PixelTravel. Take advantage of holiday offers and eliminate last-minute planning.',
+  },
+  {
+    title: 'Immersive Augmented Reality',
+    icon: '/tech.svg',
+    variant: 'green',
+    description:
+      'Experience hiking like never before with PixelTravel’s augmented reality technology. Get guided through forests and mountains, even without an internet connection.',
+  },
+  {
+    title: 'Discover New Locations Monthly',
+    icon: '/location.svg',
+    variant: 'orange',
+    description:
+      'Explore a wealth of new locations every month through PixelTravel. Our global community of travelers shares their best experiences regularly.',
+  },
+];
+
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: 'Learn More',
+    links: [
+      'About PixelTravel',
+      'Press Releases',
+      'Environment',
+      'Jobs',
+      'Privacy Policy',
+      'Contact Us',
+    ],
+  },
+  {
+    title: 'Our Community',
+    links: ['Mountain Climbing', 'Hiking PixelTravel', 'PixelTravel Hyraxhill'],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: 'Contact Us',
+  links: [
+    { label: 'Admin Officer', value: '254-740-455-200' },
+    { label: 'Email Officer', value: 'support@pixelayout.site' },
+  ],
+};
+
+export const SOCIALS = {
+  title: 'Social',
+  links: [
+    {
+      platform: '/facebook.svg',
+      link: 'https://facebook.com/pixelayout',
+    },
+    {
+      platform: '/instagram.svg',
+      link: 'https://instagram/pixelayout',
+    },
+    {
+      platform: '/twitter.svg',
+      link: 'https://x/pixelayout',
+    },
+  ],
+};
+
